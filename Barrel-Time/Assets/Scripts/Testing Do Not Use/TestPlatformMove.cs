d503a99cbd7e584d2390d18cfef9d00b58f4c7dc
Henry Smith <hgsmith@uci.edu>
2016-10-15 09:39:34 -0700

    Updated Tests

--- Barrel-Time/Assets/Scripts/Testing Do Not Use/TestPlatformMove.cs
+++ Barrel-Time/Assets/Scripts/Testing Do Not Use/TestPlatformMove.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TestPlatformMove : MonoBehaviour {
 
   public float move_speed;
+  private bool has_been_seen;
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +13,19 @@ public class TestPlatformMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    Vector3 temp_pos = transform.position;
-    temp_pos.x += move_speed * Time.deltaTime;
-    transform.position = temp_pos;
+    //Vector3 temp_pos = transform.position;
+    //temp_pos.x += move_speed * Time.deltaTime;
+    //transform.position = temp_pos;
 	}
+
+  void OnBecameInvisible() {
+    if (has_been_seen) {
+      Destroy(gameObject);
+    }
+  }
+
+  void OnBecameVisible() {
+    has_been_seen = true;
+  }
+
 }
--- Barrel-Time/Assets/Scripts/Testing Do Not Use/TestSpawnPlatforms.cs
+++ Barrel-Time/Assets/Scripts/Testing Do Not Use/TestSpawnPlatforms.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class TestSpawnPlatforms : MonoBehaviour {
 
+  public TestPlatformMove platform;
+  public float spawn_rate;
+  private float timer;
+
 	// Use this for initialization
 	void Start () {
-	
+    timer = spawn_rate;
 	}
 	
+  Vector2 polarToCoord(float theta,float radius) {
+    float y = Mathf.Sin(theta) * radius;
+    float x = Mathf.Cos(theta) * radius;
+    return new Vector2(x, y);
+  }
+
 	// Update is called once per frame
 	void Update () {
-	
+    if (timer >= spawn_rate && spawn_rate > 0) {
+      timer -= spawn_rate;
+      TestPlatformMove go = (TestPlatformMove)Instantiate(platform);
+      go.transform.parent = transform;
+      go.transform.localPosition = new Vector3(-9, 0);
+    }
+    timer += Time.deltaTime;
 	}
 }
