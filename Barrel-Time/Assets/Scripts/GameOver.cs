63f594ec8d932ccdf374cd0b798b3225698bd5b0
ravingnomad <joshbervera@yahoo.com>
2016-10-15 12:11:32 -0700

    Add Game Over Screen

---
+++ Barrel-Time/Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    private SpriteRenderer gameOver;
+	// Use this for initialization
+	void Start () {
+        gameOver = gameObject.GetComponent<SpriteRenderer>();
+        gameOver.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisplayScreen()
+    {
+        gameOver.enabled = true;
+    }
+}
