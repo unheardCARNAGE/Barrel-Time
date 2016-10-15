8c5c6ae7071a14c725082a474b55ae1af9c07e22
Henry Smith <hgsmith@uci.edu>
2016-10-14 23:33:45 -0700

    Made testing stuff

---
+++ Barrel-Time/Assets/Scripts/Testing Do Not Use/TestPlatformMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlatformMove : MonoBehaviour {
+
+  public float move_speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 temp_pos = transform.position;
+    temp_pos.x += move_speed * Time.deltaTime;
+    transform.position = temp_pos;
+	}
+}
