58e16dcebf9795d2998703d54cdb2aac7dcdf377
Paul Grad <paulgrad3rd@gmail.com>
2016-10-14 21:34:35 -0700

    Added rotation script

---
+++ Barrel-Time/Assets/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+     Vector3 rotationVector;
+     float rotationMagnitude;
+     void Awake () {
+          rotationMagnitude = 0.5f;
+          rotationVector = new Vector3(0f,rotationMagnitude,0f);
+     }
+	// Use this for initialization
+	void Start () {
+          
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          transform.Rotate(rotationVector);
+	}
+}
