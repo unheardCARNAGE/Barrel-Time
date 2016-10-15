b34829810511fb45fb51577a05b271b8a241ef04
Paul Grad <paulgrad3rd@gmail.com>
2016-10-14 22:59:13 -0700

    Added SpawnPlatform

---
+++ Barrel-Time/Assets/Scripts/SpawnPlatform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPlatform : MonoBehaviour
+{
+     public GameObject platform;
+     // Use this for initialization
+     void Start()
+     {
+          (GameObject.Instantiate(platform) as GameObject).transform.parent = transform;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     }
+}
