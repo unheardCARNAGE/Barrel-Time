561c99f65616944304980e2ad280bff8092458e6
unheardCARNAGE <timcharveyjr@hotmail.com>
2016-10-15 13:28:26 -0700

    made score update script

--- Barrel-Time/Assets/Scripts/ScoreManager.cs
+++ Barrel-Time/Assets/Scripts/ScoreManager.cs
@@ -18,7 +18,6 @@ public class ScoreManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         text.text = "Score: " + score;
-        score++;
 
     }
 }
---
+++ Barrel-Time/Assets/Scripts/ScoreUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUp : MonoBehaviour {
+
+
+
+    void OnColliderinter2D()
+    {
+        ScoreManager.score += 100;
+    }
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
