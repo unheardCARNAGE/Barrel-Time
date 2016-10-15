3e8613ad7f4c3870c9feb5d66a5a946b1cfcd614
ravingnomad <joshbervera@yahoo.com>
2016-10-15 10:24:54 -0700

    Create script that kills player when touching lava

--- Barrel-Time/Assets/Scripts/KillPlayer.cs
+++ Barrel-Time/Assets/Scripts/KillPlayer.cs
@@ -2,6 +2,19 @@
 using System.Collections;
 
 public class KillPlayer : MonoBehaviour {
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Lava")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Update()
+    {
+    }
+
 	public void OnBecameInvisible() {
           GameController.EndGame();
      }
