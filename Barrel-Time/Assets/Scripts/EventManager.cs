355ad68e3e902dac63482f1a0604c6544badac87
Henry Smith <hgsmith@uci.edu>
2016-10-16 14:46:04 -0700

    Update

---
+++ Barrel-Time/Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void loadHardMode() {
+    //Application.LoadLevel();
+  }
+
+  public void loadEasyMode() {
+    //
+  }
+
+  public void quitGame() {
+    Application.Quit();
+  }
+}
